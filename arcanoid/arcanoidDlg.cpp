0ff9a9d02369c1b36781475dd4085816d290fe7c
Nikita <basik-cs@mail.ru>
2015-10-12 15:45:57 +1000

    v 1.0

---
+++ arcanoid/arcanoidDlg.cpp
@@ -0,0 +1,272 @@
+
+// arcanoidDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "arcanoid.h"
+#include "arcanoidDlg.h"
+#include "afxdialogex.h"
+#include "math.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CarcanoidDlg dialog
+
+
+
+CarcanoidDlg::CarcanoidDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CarcanoidDlg::IDD, pParent)
+	, HELP(0)
+{
+	crd.x = 0;
+	crd.y = 0;
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CarcanoidDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, HELP);
+}
+
+BEGIN_MESSAGE_MAP(CarcanoidDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+
+// CarcanoidDlg message handlers
+
+BOOL CarcanoidDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CarcanoidDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CarcanoidDlg::OnPaint()
+{
+
+		CPaintDC dc(this);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = rect.Width();
+		int y = rect.Height();
+		
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CarcanoidDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CarcanoidDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	Player(point);
+}
+
+void CarcanoidDlg::Player(CPoint point)
+{
+	CClientDC DC(this);
+	CRect rect;
+	static double a=0; // запоминалка 
+	CPen pn;
+	pn.CreatePen(0, 10, RGB(255, 255, 255));
+	GetClientRect(&rect);
+	DC.SelectObject(pn);
+	int y = rect.Height();
+	DC.Rectangle(a - (68 / 2), y - 43, a + (68 / 2), y - 50);
+	CDC* pDC = &DC;
+	CBitmap bmp;
+	bmp.LoadBitmap(IDB_BITMAP1);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	dc.SelectObject(&bmp);
+	pDC->BitBlt(point.x-(68/2), y-50, 68, 7, &dc, 0, 0, SRCCOPY);
+	a = point.x;
+	crd.x = point.x - (68 / 2);
+	crd.y = y - 50;
+}
+
+void CarcanoidDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	static bool start = 0;
+	if (!start){
+		start = 1;
+		ball();
+	}
+	
+	
+}
+
+void CarcanoidDlg::ball()
+{
+	UpdateData(0);
+	CClientDC DC(this);
+	CRect rect;
+	CPen pn;
+	pn.CreatePen(0, 10, RGB(255, 255, 255));
+	DC.SelectObject(pn);
+	GetClientRect(&rect);
+	int x = rect.Width()/2;
+	int y = rect.Height()/2;
+	SetTimer(1, 3, 0);
+	static double alf = 90;
+	static int timer = 0;
+	static CPoint tchka(x - (12 / 2),y);
+	static int cygl = 0; // kof ygla
+
+	
+
+	///////////////////////////////////Шарик об платформу
+
+	DC.Rectangle(tchka.x + (timer - 1)*cos(3.14*alf / 180.0), tchka.y  + (timer - 1)*sin(3.14*alf / 180.0), tchka.x + 12 + (timer - 1)*cos(3.14*alf / 180.0), tchka.y +12+ (timer - 1)*sin(3.14*alf / 180.0));
+	if ((((tchka.x + timer*cos(3.14*alf / 180.0)) <= (crd.x + 68)) && ((tchka.x+12 + timer*cos(3.14*alf / 180.0)) >= crd.x)) && ((tchka.y + 12 + timer*sin(3.14*alf / 180.0)) >= crd.y) && ((tchka.y + timer*sin(3.14*alf / 180.0)) <= (crd.y + 7))){
+		tchka.x = tchka.x + timer*cos(3.14*alf / 180.0);
+		tchka.y = tchka.y + timer*sin(3.14*alf / 180.0);
+		if (((crd.x + 34) - (x + timer*cos(3.14*alf / 180.0))) >= 0) alf = -1*ygl(double(abs((crd.x + 34) - (tchka.x + timer*cos(3.14*alf / 180.0)))), double(7.0)) - 90;
+		else alf = ygl(double(abs((crd.x + 34) - (tchka.x + timer*cos(3.14*alf / 180.0)))), double(7.0)) - 90;
+		timer = 0;
+	}
+
+	//////////////////////////////////Шарик об правую стенку
+
+	if ((tchka.x + 12 + timer*cos(3.14*alf / 180.0)) >= (x * 2)){
+		tchka.x = tchka.x + timer*cos(3.14*alf / 180.0);
+		tchka.y = tchka.y + timer*sin(3.14*alf / 180.0);
+		alf *= -1;
+		alf -= 180;
+		timer = 0;
+	}
+
+	///////////////////////////////////Шарик об левую стенку
+
+	if ((tchka.x + timer*cos(3.14*alf / 180.0)) <= 0){
+		tchka.x = tchka.x + timer*cos(3.14*alf / 180.0);
+		tchka.y = tchka.y + timer*sin(3.14*alf / 180.0);
+		alf *= -1;
+		alf += 180;
+		timer = 0;
+	}
+
+	/////////////////////////////////////Шарик об потолок
+
+	if ((tchka.y + timer*sin(3.14*alf / 180.0)) <= 0){
+		tchka.x = tchka.x + timer*cos(3.14*alf / 180.0);
+		tchka.y = tchka.y + timer*sin(3.14*alf / 180.0);
+		alf *= -1;
+		timer = 0;
+	}
+
+	HELP = alf;
+	CDC* pDC = &DC;
+	CBitmap bmp;
+	bmp.LoadBitmap(IDB_BITMAP2);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	dc.SelectObject(&bmp);
+	pDC->BitBlt(tchka.x + timer*cos(3.14*alf / 180.0), tchka.y + timer*sin(3.14*alf / 180.0), 12, 12, &dc, 0, 0, SRCCOPY);
+	timer+=1;
+	
+
+}
+
+double CarcanoidDlg::ygl(double a, double b)
+{
+	UpdateData(0);
+	double ygl = double(atan(a / b)*180.0/3.14);
+	//HELP = ygl;
+	return ygl;
+}
+
+
+void CarcanoidDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	ball();
+}
---
+++ arcanoid/arcanoidDlg.h
@@ -0,0 +1,41 @@
+
+// arcanoidDlg.h : header file
+//
+
+#pragma once
+
+
+// CarcanoidDlg dialog
+class CarcanoidDlg : public CDialogEx
+{
+// Construction
+public:
+	CarcanoidDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_ARCANOID_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	void CarcanoidDlg::Player(CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	void CarcanoidDlg::ball();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	double CarcanoidDlg::ygl(double a, double b);
+	CPoint crd; // координаты платформы (н.л. угл)
+	double HELP;
+};
